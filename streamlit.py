2a7827cf84f36cec3570f8dadd2fde0875a9fdd8
soprusty <111330999+soprusty@users.noreply.github.com>
2022-10-29 10:39:48 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,51 +1,25 @@
-#import xgboost as xgb
 import streamlit as st
-#import pandas as pd
-
-#Loading up the Regression model we created
-#model = xgb.XGBRegressor()
-#model.load_model('xgb_model.json')
-
-#Caching the model for faster loading
-@st.cache
-
-
-# Define the prediction function
-def predict(carat, cut, color, clarity, depth, table, x, y, z,etext):
-    #Predicting the price of the carat
-    
-
- #   prediction = model.predict(pd.DataFrame([[carat, cut, color, clarity, depth, table, x, y, z]], columns=['carat', 'cut', 'color', 'clarity', 'depth', 'table', 'x', 'y', 'z']))
- #   return prediction
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 
 #st.title('Customer Transaction Predictor')
 ##st.image("""https://www.india.com/wp-content/uploads/2014/08/666.jpg""")
 #st.header('Enter the characteristics of the Customer:')
 etext = st.text_area("Enter Something here! ")
-
-wordcloud = WordCloud(width = 800, height = 800,
-                background_color ='white',
-                stopwords = stopwords,
-                min_font_size = 10).generate(etext)
-
-plt.figure(figsize = (8, 8), facecolor = None)
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.tight_layout(pad = 0)
-plt.show()
-
 st.write(etext)
-carat = st.number_input('Carat Weight:', min_value=0.1, max_value=10.0, value=1.0)
-cut = st.selectbox('Cut Rating:', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
-color = st.selectbox('Color Rating:', ['J', 'I', 'H', 'G', 'F', 'E', 'D'])
-clarity = st.selectbox('Clarity Rating:', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
-depth = st.number_input('Diamond Depth Percentage:', min_value=0.1, max_value=100.0, value=1.0)
-table = st.number_input('Diamond Table Percentage:', min_value=0.1, max_value=100.0, value=1.0)
-x = st.number_input('Diamond Length (X) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-y = st.number_input('Diamond Width (Y) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-z = st.number_input('Diamond Height (Z) in mm:', min_value=0.1, max_value=100.0, value=1.0)
+wordcloud = WordCloud().generate(etext)
 
-if st.button('Predict Customer Transaction'):
-    #price = predict(carat, cut, color, clarity, depth, table, x, y, z)
-    st.success(f'The predicted price of the diamond is ${price[0]:.2f} USD')
+##wordcloud = WordCloud(width = 800, height = 800,
+ #               background_color ='white',
+  #              stopwords = stopwords,
+   #             min_font_size = 10).generate(etext)
+
+##plt.figure(figsize = (8, 8), facecolor = None)
+##plt.imshow(wordcloud)
+##plt.tight_layout(pad = 0)
+
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+st.pyplot()
