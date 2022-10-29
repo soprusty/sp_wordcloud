2d3691fb88b0edbba54711796a47261328e66c00
soprusty <111330999+soprusty@users.noreply.github.com>
2022-10-29 10:41:08 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,23 +2,13 @@ import streamlit as st
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+# Create some sample text
+text = 'Fun, fun, awesome, awesome, tubular, astounding, superb, great, amazing, amazing, amazing, amazing'
 
-#st.title('Customer Transaction Predictor')
-##st.image("""https://www.india.com/wp-content/uploads/2014/08/666.jpg""")
-#st.header('Enter the characteristics of the Customer:')
-etext = st.text_area("Enter Something here! ")
-st.write(etext)
-wordcloud = WordCloud().generate(etext)
+# Create and generate a word cloud image:
+wordcloud = WordCloud().generate(text)
 
-##wordcloud = WordCloud(width = 800, height = 800,
- #               background_color ='white',
-  #              stopwords = stopwords,
-   #             min_font_size = 10).generate(etext)
-
-##plt.figure(figsize = (8, 8), facecolor = None)
-##plt.imshow(wordcloud)
-##plt.tight_layout(pad = 0)
-
+# Display the generated image:
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
