9483202d103fc4bcacaea37e0af1e66a3c22d9fd
soprusty <111330999+soprusty@users.noreply.github.com>
2022-10-29 15:00:28 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,7 +6,7 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install',
 'wordcloud'])
 
 import streamlit as st
-from wordcloud import WordCloud
+from wordcloud import WordCloud,STOPWORDS
 import matplotlib.pyplot as plt
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
@@ -15,7 +15,7 @@ st.subheader('wordcloud app')
 text=st.text_input('enter text')
 
 if text:
-  w=WordCloud(background_color='white').generate(text)
+  w=WordCloud(background_color='white',stopwords = STOPWORDS).generate(text)
   plt.axis("off")
   plt.imshow(w,interpolation='bilinear')
   #plt.imshow(wordcloud, interpolation='bilinear')
